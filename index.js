341320da739e641ca27b8addf09c55a6f060c7fd
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-06-20 18:21:53 +0200

    hamburger menu toggle working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
+    console.log("clicked, bitch!")
     document.body.classList.toggle('nav-open');
 });
 
